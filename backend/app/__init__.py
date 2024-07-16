f9dcb297e94f550fbd5355d9178ee1838b04a109
loganphillips792 <loganphillips792@gmail.com>
2024-07-15 22:43:50 -0500

    fix linting error

---
+++ backend/app/__init__.py
@@ -0,0 +1,3 @@
+# created this file for pylint error:
+# backend/app/__init__.py:1:0: F0010: error while code parsing: Unable to load file backend/app/__init__.py:
+# [Errno 2] No such file or directory: 'backend/app/__init__.py' (parse-error)
